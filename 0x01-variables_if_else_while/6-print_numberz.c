64fe52eb604773f75deea4bfefdaafef2eb8821d
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-17 10:54:55 -0700

    print number putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
-	printf("\n");
+	putchar('\n');
 return (0);
 }
