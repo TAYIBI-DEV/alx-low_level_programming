739a1d90f575597be162fa63067a007a534c5080
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-17 18:03:53 -0700

    print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,17 @@ int main(void)
 
 	for (i = '0'; i <= '9' ; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = '1'; j <= '9'; j++)
 		{
+			if ( j > i)
+			{
 			putchar(i);
 			putchar(j);
-			if (i != '9' || j != '9')
+			if (i != '8' || j != '9')
 			{
 			putchar(',');
 			putchar(' ');
+			}
 			}
 		}
 	}
