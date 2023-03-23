0cc276c5cae46d465958936426c2c84ad3c25cb1
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-23 10:36:20 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- prints a numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+}
