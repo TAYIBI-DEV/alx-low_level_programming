5f20e8553b900e268144b563e46b9d6c70622f94
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-23 14:12:42 -0700

    print diago

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,14 +16,12 @@ void print_diagonal(int n)
 
 		for (i = 0; i < n; i++)
 		{
-	
 			for (j = 0; j < n; j++)
 			{
 				if (i == j)
 					_putchar('\\');
-		
 				else if (j < i)
-			_putchar(' ');
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
