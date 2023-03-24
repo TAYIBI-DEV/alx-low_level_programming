1a0e6442b30603736bbc992d816109b9d7d344f4
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-23 19:00:08 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	long int n;
+	long int prime;
+	int i;
+
+	n = 612852475143;
+	prime = -1;
+
+	while (n % 2 == 0)
+	{
+		prime = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			prime = i;
+			n = n / i;
+		}
+	}
+	if (n < 2)
+		prime = n;
+	printf("%ld\n", prime);
+	return (0);
+}
