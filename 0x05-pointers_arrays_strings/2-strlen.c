ac1cc06a1dfd6ffb09ff306253bb62f37b936c1b
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-27 09:52:21 -0700

    return a len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function return lenght of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	len = strlen(s);
+
+	return (len);
+}
