132a78f52ced7fa81abb6b8fb5db6fd05f0c0b5a
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-27 15:40:12 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - functin prints other caracter stating with the first one
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int c = strlen(str) - 1;
+
+		for (i = 0; i < c; i++)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	_putchar('\n');
+}
+
