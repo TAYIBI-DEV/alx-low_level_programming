0f3282129b39e6687ba1651d3e9b2ac84f2499ea
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-27 12:39:19 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
