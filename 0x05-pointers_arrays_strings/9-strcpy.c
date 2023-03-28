344c3563ed56939a0f608041dba8b99fea7512f6
Mohamed Tayibi <tayibi.m@gmail.com>
2023-03-27 17:00:43 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copy
+ * @dest: string
+ * @src: string source
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *c;
+
+	c = strcpy(dest, src);
+	return (c);
+}
