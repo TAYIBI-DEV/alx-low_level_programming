7fbd64ed89c136c3cfba71c9a3e6be888a937a42
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-10 11:42:35 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: string
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
