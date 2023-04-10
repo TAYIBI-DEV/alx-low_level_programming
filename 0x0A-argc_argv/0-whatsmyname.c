6746439fe7634c5487c054f652ec903a4e935423
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-10 11:17:36 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: string
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
