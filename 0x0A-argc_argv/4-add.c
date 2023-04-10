4246ed3dddf4cd9b9605a56f26bfb0cf583de928
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-10 14:11:31 -0700

    add digits

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * is_valid_number - The Function
+ * @s:  The variable
+ * Return: The Return value/void
+ */
+
+
+int is_digit(char *s)
+{
+int i = 0;
+
+	while (s[i])
+	{
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+
+}
+
+
+
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: string
+ * Return: always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_digit(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		if (argc > 1)
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
