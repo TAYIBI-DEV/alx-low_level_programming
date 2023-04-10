77be4d8e608a89fd42a403a2bebc7c2a5ad36d83
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-10 12:33:50 -0700

    multip

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: string
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int mult = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n",mult);
+	}
+	return (0);
+}
