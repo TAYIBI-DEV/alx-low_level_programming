10f292413534b1edd19b3016cecd9418035b8993
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-11 16:08:53 -0700

    concatenat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * str_concat - function that concatenates two string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: not void
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len1, len2;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	p = malloc(sizeof(char) * (len1 + len2));
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++, i++)
+		p[i] = s2[j];
+	p[i] = '\0';
+	return (p);
+}
