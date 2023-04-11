251fdb474cb85299942b64d7d3fbf47449e18a32
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-10 19:40:59 -0700

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: string
+ * Return: always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	int total, count;
+	unsigned int i;
+	char *p;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(coins[i]); i++)
+			{
+				if (total >= coins[i])
+				{
+					count += total / coins[i];
+					total = total % coins[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
+
+
