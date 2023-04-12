299ec6e3526ed5f85f49d8abf799106521b8089d
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-11 18:46:37 -0700

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,13 +24,13 @@ int _strlen(char *s)
 
 char *argstostr(int ac, char **av)
 {
-	int i, size, j, count = 0;
+	int i = 0, size = 0, j, count = 0;
 	char *s;
 
 	if (ac == 0 || av == 0)
 		return (NULL);
 
-	for (i = 0; i < ac; i++, size++)
+	for (; i < ac; i++, size++)
 		size += _strlen(av[i]);
 	s = malloc(sizeof(char) * size + 1);
 	if (s == 0)
@@ -38,9 +38,8 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++, count++)
-		{
 			s[count] = av[i][j];
-		}
+
 		s[count] = '\n';
 		count++;
 	}
