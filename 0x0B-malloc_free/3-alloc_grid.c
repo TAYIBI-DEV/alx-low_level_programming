d3f9244be39b4e0a9fafa7aefa896871be2c65a1
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-11 17:09:31 -0700

    last alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int) * width);
+		p[i] = malloc(sizeof(int *) * width);
 
 			if (p[i] == NULL)
 			{
