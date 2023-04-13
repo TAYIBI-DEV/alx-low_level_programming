b1ee42611acb4e4f388febc8044b656833959a48
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-13 15:19:47 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -32,12 +32,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	p = malloc(sizeof(int) * nmemb + 1);
+	p = malloc(sizeof(int) * nmemb);
 
 	if (p == 0)
 		return (NULL);
 
-	_memset(p, 0, sizeof(int) * nmemb);
+	_memset(p, 0, sizeof(int) * nmemb - 1);
 
 	return (p);
 }
