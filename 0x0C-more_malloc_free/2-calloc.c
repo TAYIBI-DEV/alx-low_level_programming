cef9fa5a9e8b56a8cadf331d3824b054da679622
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-13 14:43:09 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _calloc - function that allocates memory
+ * @nmemb: int
+ * @size: int
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(sizeof(int) * nmemb);
+
+	if (p == 0)
+		return (NULL);
+	memset(p, 0, sizeof(int) * nmemb);
+	return (p);
+}
