e7cd6bd9d5ec00c16f896af0baf6f82fe5f575c7
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-17 15:39:57 -0700

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * free_dog - function free dogs
+ * @d: dog
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
