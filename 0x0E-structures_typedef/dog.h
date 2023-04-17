51d0da0fe908e86d906cc865a01f3f72110786b8
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-17 12:58:40 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - a dog structer
+ * @name: char
+ * @age: int
+ * @owner: char
+ *
+ * Description: dog information
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
