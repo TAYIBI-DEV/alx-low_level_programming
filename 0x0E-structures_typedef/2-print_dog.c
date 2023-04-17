680a9db1413b9a392c6fb8a7aecb3d99ebb435d8
Mohamed Tayibi <tayibi.m@gmail.com>
2023-04-17 14:02:44 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)");
 		}
 			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)");
-		}
+			printf("Age: %f\n", d->age);
 			printf("Owner: %s\n", d->owner);
 	}
 }
