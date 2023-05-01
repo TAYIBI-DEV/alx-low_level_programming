afc7ba0c9133c3b670a6d9fd205c3158d47443fa
Mohamed Tayibi <tayibi.m@gmail.com>
2023-05-01 13:50:43 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - function print all element listint_t
+ * @h: pointer
+ * Return: numbr of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
