91a3df3a51b216c1f28fa74bc978466eb13f0428
TAYIBI-DEV <tayibi.m@gmail.com>
2023-05-08 16:58:58 -0700

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - function read text fils
+ * @filename: file
+ * @letters: size of letter
+ * Return: nbr of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fp;
+	char *buff;
+	ssize_t r, w;
+
+	fp = open(filename, O_RDONLY);
+	if (fp < 0)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	r = read(fp, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+	if (!w)
+		return (0);
+
+	free(buff);
+	close(fp);
+	return (r);
+}
+
+
